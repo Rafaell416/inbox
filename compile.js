265fd4ec8e0b65f8a58aaba636c4c283c0c7adcd
Rafel Villarreal <rvillarreal416@gmail.com>
2018-07-23 22:36:34 -0500

    bugfix: test if exist a default message

--- compile.js
+++ compile.js
@@ -5,6 +5,4 @@ const solc = require('solc')
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
 const source = fs.readFileSync(inboxPath, 'utf8')
 
-const contract = solc.compile(source, 1)
-
-console.log(contract)
+module.exports = solc.compile(source, 1).contracts[':Inbox']
